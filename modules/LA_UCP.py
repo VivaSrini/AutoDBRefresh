b3b8d4fd34a0746455f3f26254e384e4365024e6
Srinivas Gopalakrishnan <srinig@viva-it.com>
2023-06-02 15:22:59 +0530

    Adding modules LA_UCP and MA_BOSTON

--- modules/LA_UCP.py
+++ modules/LA_UCP.py
@@ -9,3 +9,130 @@
 
 # Field Mapping:
 #
+# ╔═══════════════════════════╦══════════════════╦════════════╗
+# ║ DEST_COL_NAME             ║ SRC_COL_NAME     ║ SRC_COL_NO ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Sequence                  ║ Program          ║  Generated ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Company Name              ║ VENDOR_NAME      ║      2     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ DBA                       ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ DUNS Number               ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Addr Line 1               ║ MAIL_ADDR_STREET ║      5     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Addr Line 2               ║ MAIL_ADDR_AUX    ║      6     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ City                      ║ MAIL_CITY        ║      9     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ State                     ║ MAIL_STATE       ║     10     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Zip Code                  ║ MAIL_ZIP_CODE    ║     11     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Website                   ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Contact Person            ║ OWNER_NAME       ║     14     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Phone                     ║ PHONE_NUM        ║     12     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Cell Phone                ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Fax                       ║ FAX_NUM          ║     13     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Email Address             ║ EMAIL_ADDR       ║     15     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Gender                    ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Ethnicity                 ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Given Diversity           ║ MINORITY_TYPE    ║      3     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Primary Diversity         ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Other Diversities         ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Certifying Agency         ║ CERT_AGENCY      ║     16     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Certification Number      ║ VENDOR_NUM       ║      1     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Certification Start Date  ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Certification Expiry Date ║ EXPIRATION_DATE  ║      4     ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Primary NAICS Code        ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Other NAICS Codes         ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Business Description      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 1      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 2      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 3      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 4      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 5      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 6      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 7      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 8      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 9      ║                  ║            ║
+# ╠═══════════════════════════╬══════════════════╬════════════╣
+# ║ Additional Details 10     ║                  ║            ║
+# ╚═══════════════════════════╩══════════════════╩════════════╝
+#
+
+import csv
+import logging
+
+from src import custom_logger, prepare_csv_data, dedup_csv, empty_dict
+from src import get_primary, trim_file_header_rows
+
+log = custom_logger.LogGen.logGen()
+log.setLevel(level=logging.DEBUG)
+
+provider = "LA_UCP"
+records = []
+
+
+def processFile(filename):
+    log.info(f'Loaded module {provider}...')
+
+    trim_file_header_rows(file_name=filename, num_lines=2)
+    columns_to_be_removed = ['WORK_CODE', 'WORK_DESCRIPTION']
+    dedup_csv(filename, columns_to_be_removed)
+
+    with open(filename, encoding="utf8") as f:
+        reader = csv.reader(f)
+        next(reader, None)     # CSV Header
+
+        for row_num, row in enumerate(reader):
+            dict1 = empty_dict()
+            dict1['source'] = provider
+            dict1['sequence'] = prepare_csv_data(row_num + 1)
+            dict1['company'] = prepare_csv_data(row[1])
+            dict1['addr1'] = prepare_csv_data(row[4])
+            dict1['addr2'] = prepare_csv_data(row[5])
+            dict1['city'] = prepare_csv_data(row[8])
+            dict1['state'] = prepare_csv_data(row[9])
+            dict1['zipcode'] = prepare_csv_data(row[10])
+            dict1['contact'] = prepare_csv_data(row[13])
+            dict1['phone'] = prepare_csv_data(row[11])
+            dict1['fax'] = prepare_csv_data(row[12])
+            dict1['email'] = prepare_csv_data(row[14])
+            dict1['ethnicity'] = prepare_csv_data(get_primary(row[6]))
+            dict1['gdiverse'] = prepare_csv_data(get_primary(row[2]))
+            dict1['cert_agency'] = prepare_csv_data(row[15])
+            dict1['cert_num'] = prepare_csv_data(row[0])
+            dict1['cert_expiry'] = prepare_csv_data(row[3])
+            dict1['addl1'] = prepare_csv_data(row[7], prefix='License Number(s)')
+            dict1['addl2'] = prepare_csv_data(row[16], prefix='Service Type(s)')
+            records.append(dict1)
+
+    return records
--- modules/MA_BOSTON.py
+++ modules/MA_BOSTON.py
@@ -6,4 +6,141 @@
 #    "Title": "Certified Business Directory - Datasets - Analyze Boston",
 #    "ImportedFileName": "tmpj8geo5py.csv"
 #  }
+#
+# ╔═══════════════════════════╦══════════════════════╦════════════╗
+# ║ DEST_COL_NAME             ║ SRC_COL_NAME         ║ SRC_COL_NO ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Sequence                  ║ Program              ║  Generated ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Company Name              ║ company_name         ║      1     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ DBA                       ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ DUNS Number               ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Addr Line 1               ║ address              ║      8     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Addr Line 2               ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ City                      ║ city                 ║      9     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ State                     ║ state                ║     10     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Zip Code                  ║ zipcode              ║     11     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Website                   ║ website              ║     18     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Contact Person            ║ contact_name         ║     12     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Phone                     ║ phone                ║     14     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Cell Phone                ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Fax                       ║ fax                  ║     15     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Email Address             ║ email                ║     16     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Gender                    ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Ethnicity                 ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Given Diversity           ║ mbe_wbe_cert,        ║    3,4,5   ║
+# ║                           ║ small_local_cert,    ║            ║
+# ║                           ║ veteran_cert         ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Primary Diversity         ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Other Diversities         ║                      ║     6,7    ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Certifying Agency         ║ 'City of Boston, MA' ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Certification Number      ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Certification Start Date  ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Certification Expiry Date ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Primary NAICS Code        ║ naics_codes_1        ║     25     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Other NAICS Codes         ║ naics_codes_2,       ║    26,27   ║
+# ║                           ║ naics_codes_3        ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Business Description      ║ services_provided    ║      2     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 1      ║ contact_title        ║     13     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 2      ║ business_type        ║     18     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 3      ║ construction         ║     19     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 4      ║ date_business_estd   ║     20     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 5      ║ number_of_employees  ║     21     ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 6      ║ cob_category_codes   ║    22-24   ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 7      ║ unspsc_codes         ║    28-30   ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 8      ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 9      ║                      ║            ║
+# ╠═══════════════════════════╬══════════════════════╬════════════╣
+# ║ Additional Details 10     ║                      ║            ║
+# ╚═══════════════════════════╩══════════════════════╩════════════╝
+#
 
+import csv
+import logging
+
+from src import custom_logger, prepare_csv_data, empty_dict
+
+log = custom_logger.LogGen.logGen()
+log.setLevel(level=logging.DEBUG)
+
+provider = "MA_BOSTON"
+records = []
+
+
+def processFile(filename):
+    log.info(f'Loaded module {provider}...')
+
+    with open(filename, encoding="utf8") as f:
+        reader = csv.reader(f)
+        next(reader, None)     # CSV Header
+
+        for row_num, row in enumerate(reader):
+            dict1 = empty_dict()
+            dict1['source'] = provider
+            dict1['sequence'] = prepare_csv_data(row_num + 1)
+            dict1['company'] = prepare_csv_data(row[0])
+            dict1['addr1'] = prepare_csv_data(row[7])
+            dict1['city'] = prepare_csv_data(row[8])
+            dict1['state'] = prepare_csv_data(row[9])
+            dict1['zipcode'] = prepare_csv_data(row[10])
+            dict1['website'] = prepare_csv_data(row[16])
+            dict1['contact'] = prepare_csv_data(row[11])
+            dict1['phone'] = prepare_csv_data(row[13])
+            dict1['fax'] = prepare_csv_data(row[14])
+            dict1['email'] = prepare_csv_data(row[15])
+            dict1['gdiverse'] = prepare_csv_data(','.join([row[2], row[3], row[4]]))
+            dict1['cert_agency'] = 'City of Boston, MA'
+            dict1['pnaics'] = prepare_csv_data(row[24], pattern='^[^ ]*')
+            dict1['onaics'] = ','.join([prepare_csv_data(row[25], pattern='^[0-9]{6}'),
+                                        prepare_csv_data(row[26], pattern='^[0-9]{6}')])
+            dict1['biz_desc'] = prepare_csv_data(row[1])
+            dict1['addl1'] = prepare_csv_data(row[12], prefix='Contact Title')
+            dict1['addl2'] = prepare_csv_data(row[17], prefix='Business Type')
+            dict1['addl3'] = prepare_csv_data(row[18], prefix='Construction?')
+            dict1['addl4'] = prepare_csv_data(row[19], prefix='Date Business Estd.')
+            dict1['addl5'] = prepare_csv_data(row[20], prefix='No.of Employees')
+            dict1['addl6'] = prepare_csv_data(','.join([prepare_csv_data(row[21]),
+                                                        prepare_csv_data(row[22]),
+                                                        prepare_csv_data(row[23])]),
+                                              prefix='Cob Category Codes')
+            dict1['addl7'] = prepare_csv_data(','.join([prepare_csv_data(row[21], pattern='^[0-9]{8}'),
+                                                        prepare_csv_data(row[22], pattern='^[0-9]{8}'),
+                                                        prepare_csv_data(row[23], pattern='^[0-9]{8}')]),
+                                              prefix='UNSPSC Codes')
+            records.append(dict1)
+
+    return records
