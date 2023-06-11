d09dc60f95be34d1ac31b6b0bc7d51aff364837f
Srinivas Gopalakrishnan <srinig@viva-it.com>
2023-06-11 16:50:07 +0530

    Adding 10 more Manual Download modules
    - Downloaded catptcha-protected source files manually
    - Mapped modules directly in code without documentation
    (Header Mapping Info documentation to be added later)

---
+++ modules/WA_OMWBE.py
@@ -0,0 +1,67 @@
+# Source Info:
+#   {
+#     "Provider": "WA_OMWBE",
+#     "State": "WA",
+#     "Name": "Office of Minority and Women's Business Enterprises",
+#     "Website": "https://omwbe.diversitycompliance.com/FrontEnd/SearchCertifiedDirectory.asp",
+#     "Title": "Certified Vendor Directory",
+#     "Download": "Manual",
+#     "FileName": "WA_OMWBE.csv",
+#   }
+#
+
+import csv
+import logging
+
+from src import custom_logger, prepare_csv_data, empty_dict
+from src import trim_file_header_rows, get_pnaics_onaics
+
+log = custom_logger.LogGen.logGen()
+log.setLevel(level=logging.DEBUG)
+
+provider = "WA_OMWBE"
+records = []
+
+
+def processFile(filename):
+    log.info(f'Loaded module {provider}...')
+
+    trim_file_header_rows(file_name=filename, num_lines=5)
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader, None)     # CSV Header
+
+        for row_num, row in enumerate(reader):
+            if not row: break
+            dict1 = empty_dict()
+            dict1['source'] = provider
+            dict1['sequence'] = prepare_csv_data(row_num + 1)
+            dict1['company'] = prepare_csv_data(row[0])
+            dict1['dba'] = prepare_csv_data(row[1])
+            dict1['addr1'] = prepare_csv_data(row[4])
+            dict1['city'] = prepare_csv_data(row[5])
+            dict1['state'] = prepare_csv_data(row[6])
+            dict1['zipcode'] = prepare_csv_data(row[7])
+            dict1['website'] = prepare_csv_data(row[15])
+            dict1['contact'] = f'{prepare_csv_data(row[2])} {prepare_csv_data(row[3])}'
+            dict1['phone'] = prepare_csv_data(row[12])
+            dict1['fax'] = prepare_csv_data(row[13])
+            dict1['email'] = prepare_csv_data(row[14])
+            dict1['gdiverse'] = prepare_csv_data(row[17])
+            dict1['cert_agency'] = prepare_csv_data(row[16])
+            dict1['cert_num'] = prepare_csv_data(row[22])
+            (pnaics, onaics) = get_pnaics_onaics(row[23])
+            dict1['pnaics'] = pnaics
+            dict1['onaics'] = prepare_csv_data(onaics)
+            dict1['addl1'] = prepare_csv_data(row[8], prefix='Mailing Address')
+            dict1['addl2'] = prepare_csv_data(row[9], prefix='Mail Addr City')
+            dict1['addl3'] = prepare_csv_data(row[10], prefix='Mail Addr State')
+            dict1['addl4'] = prepare_csv_data(row[11], prefix='Mail Addr Zip')
+            dict1['addl5'] = prepare_csv_data(row[18], prefix='Capabilities')
+            dict1['addl6'] = prepare_csv_data(row[19], prefix='UDBE')
+            dict1['addl7'] = prepare_csv_data(row[20], prefix='SBE Certification')
+            dict1['addl8'] = prepare_csv_data(row[21], prefix='UBI#')
+
+            records.append(dict1)
+
+    return records
